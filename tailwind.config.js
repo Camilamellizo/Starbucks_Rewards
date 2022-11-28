80284884bd5a288fba6c53e79e1bcb1b3502b722
camila <112856775+Camilamellizo@users.noreply.github.com>
2022-11-28 22:13:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'fondo': "url('/Starbucks_Rewards/assets/star.png')",
+
+      }
+    },
+  },
+  plugins: [],
+}
